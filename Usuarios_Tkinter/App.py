704af588a6b95c1ac883e98a1557dc892237acf2
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2024-09-05 05:47:16 -0300

    Update App.py

--- Usuarios_Tkinter/App.py
+++ Usuarios_Tkinter/App.py
@@ -1,53 +1,63 @@
-from Usuarios import Usuarios  # Importa a classe que controla os dados dos usuários
-from tkinter import *  # Importa todos os componentes da biblioteca tkinter
+from Usuarios import Usuarios # Importa a classe que controla os dados dos usuários
+from tkinter import * # Importa todos os componentes da biblioteca tkinter
 
 class Application:
     def __init__(self, master=None):
-        self.fonte = ("Verdana", "8")  # Define a fonte padrão para o aplicativo
-        
-        # Criação de containers para organizar a interface gráfica
+        # Define a fonte padrão para o aplicativo
+        self.fonte = ("Verdana", "8")
+
+    # Container 1
+    # Criação de containers para organizar a interface gráfica
         self.container1 = Frame(master)
-        self.container1["pady"] = 10  # Define padding vertical
+        self.container1["pady"] = 10
         self.container1.pack()
 
-        # Cria e configura cada container (área da interface que agrupa elementos)
+    # Container 2
+    # Cria e configura cada container (área da interface que agrupa elementos)
         self.container2 = Frame(master)
         self.container2["padx"] = 20
         self.container2["pady"] = 5
         self.container2.pack()
 
-        # Continua criando outros containers, até o container9...
-        # Esses containers organizam os elementos da interface gráfica
+    # Container 3
+    # Continua criando outros containers, até o container9...
+    # Esses containers organizam os elementos da interface gráfica
         self.container3 = Frame(master)
         self.container3["padx"] = 20
         self.container3["pady"] = 5
         self.container3.pack()
 
+    # Container 4
         self.container4 = Frame(master)
         self.container4["padx"] = 20
         self.container4["pady"] = 5
         self.container4.pack()
 
+    # Container 5
         self.container5 = Frame(master)
         self.container5["padx"] = 20
         self.container5["pady"] = 5
         self.container5.pack()
 
+    # Container 6
         self.container6 = Frame(master)
         self.container6["padx"] = 20
         self.container6["pady"] = 5
         self.container6.pack()
 
+    # Container 7
         self.container7 = Frame(master)
         self.container7["padx"] = 20
         self.container7["pady"] = 5
         self.container7.pack()
 
+    # Container 8
         self.container8 = Frame(master)
         self.container8["padx"] = 20
         self.container8["pady"] = 10
         self.container8.pack()
 
+    # Container 9
         self.container9 = Frame(master)
         self.container9["padx"] = 15
         self.container9.pack()
@@ -57,17 +67,17 @@ class Application:
         self.titulo["font"] = ("Calibri", "9", "bold")
         self.titulo.pack()
 
-        # Campos de texto e botões para inserir, buscar e manipular os dados de usuário
+         # Campos de texto e botões para inserir, buscar e manipular os dados de usuário
         self.lblidusuario = Label(self.container2, text="idUsuario:", font=self.fonte, width=10)
         self.lblidusuario.pack(side=LEFT)
 
-        self.txtidusuario = Entry(self.container2)  # Campo de entrada do id do usuário
+        self.txtidusuario = Entry(self.container2) # Campo de entrada do id do usuário
         self.txtidusuario["width"] = 10
         self.txtidusuario["font"] = self.fonte
         self.txtidusuario.pack(side=LEFT)
 
         # Botão para buscar usuário no banco de dados
-        self.btnBuscar = Button(self.container2, text="Buscar", font=self.fonte, width=10)
+        self.btnBuscar = Button(self.container2, text="Buscar",font=self.fonte, width=10)
         self.btnBuscar["command"] = self.buscarUsuario
         self.btnBuscar.pack(side=RIGHT)
 
@@ -75,7 +85,7 @@ class Application:
         self.lblnome = Label(self.container3, text="Nome:", font=self.fonte, width=10)
         self.lblnome.pack(side=LEFT)
 
-        self.txtnome = Entry(self.container3)  # Campo de entrada do nome do usuário
+        self.txtnome = Entry(self.container3) # Campo de entrada do nome do usuário
         self.txtnome["width"] = 25
         self.txtnome["font"] = self.fonte
         self.txtnome.pack(side=LEFT)
@@ -83,7 +93,7 @@ class Application:
         self.lbltelefone = Label(self.container4, text="Telefone:", font=self.fonte, width=10)
         self.lbltelefone.pack(side=LEFT)
 
-        self.txttelefone = Entry(self.container4)  # Campo de entrada do telefone
+        self.txttelefone = Entry(self.container4) # Campo de entrada do Telefone
         self.txttelefone["width"] = 25
         self.txttelefone["font"] = self.fonte
         self.txttelefone.pack(side=LEFT)
@@ -91,10 +101,27 @@ class Application:
         self.lblemail = Label(self.container5, text="E-mail:", font=self.fonte, width=10)
         self.lblemail.pack(side=LEFT)
 
-        self.txtemail = Entry(self.container5)  # Campo de entrada do e-mail
+        self.txtemail = Entry(self.container5) # Campo de entrada do E-mail
         self.txtemail["width"] = 25
         self.txtemail["font"] = self.fonte
         self.txtemail.pack(side=LEFT)
+
+        self.lblusuario = Label(self.container6, text="Usuário:", font=self.fonte, width=10)
+        self.lblusuario.pack(side=LEFT)
+
+        self.txtusuario = Entry(self.container6) # Campo de entrada do Usuário
+        self.txtusuario["width"] = 25
+        self.txtusuario["font"] = self.fonte
+        self.txtusuario.pack(side=LEFT)
+
+        self.lblsenha = Label(self.container7, text="Senha:", font=self.fonte, width=10)
+        self.lblsenha.pack(side=LEFT)
+
+        self.txtsenha = Entry(self.container7) # Campo de entrada da Senha
+        self.txtsenha["width"] = 25
+        self.txtsenha["show"] = "*"
+        self.txtsenha["font"] = self.fonte
+        self.txtsenha.pack(side=LEFT)
 
         # Adiciona botões para manipular os dados no banco (inserir, alterar e excluir)
         self.bntInsert = Button(self.container8, text="Inserir", font=self.fonte, width=12)
@@ -117,14 +144,15 @@ class Application:
     # Funções para inserir, alterar, excluir e buscar dados
     def inserirUsuario(self):
         user = Usuarios()
-        user.idusuario = self.txtidusuario.get()  # Coleta os dados dos campos
+
+        user.idusuario = self.txtidusuario.get() # Coleta os dados dos campos
         user.nome = self.txtnome.get()
         user.telefone = self.txttelefone.get()
         user.email = self.txtemail.get()
         user.usuario = self.txtusuario.get()
         user.senha = self.txtsenha.get()
 
-        self.lblmsg["text"] = user.insertUser()  # Chama o método para inserir o usuário no banco de dados
+        self.lblmsg["text"] = user.insertUser() # Chama o método para inserir o usuário no banco de dados
 
         # Limpa os campos após a inserção
         self.txtidusuario.delete(0, END)
@@ -134,16 +162,18 @@ class Application:
         self.txtusuario.delete(0, END)
         self.txtsenha.delete(0, END)
 
+
     def alterarUsuario(self):
         user = Usuarios()
+
         user.idusuario = self.txtidusuario.get()
         user.nome = self.txtnome.get()
         user.telefone = self.txttelefone.get()
         user.email = self.txtemail.get()
         user.usuario = self.txtusuario.get()
         user.senha = self.txtsenha.get()
-
-        self.lblmsg["text"] = user.updateUser()  # Chama o método para alterar o usuário no banco de dados
+ 
+        self.lblmsg["text"] = user.updateUser() # Chama o método para alterar o usuário no banco de dados
 
         # Limpa os campos após a alteração
         self.txtidusuario.delete(0, END)
@@ -155,9 +185,10 @@ class Application:
 
     def excluirUsuario(self):
         user = Usuarios()
+
         user.idusuario = self.txtidusuario.get()
 
-        self.lblmsg["text"] = user.deleteUser()  # Chama o método para excluir o usuário no banco de dados
+        self.lblmsg["text"] = user.deleteUser() # Chama o método para excluir o usuário no banco de dados
 
         # Limpa os campos após a exclusão
         self.txtidusuario.delete(0, END)
@@ -167,11 +198,13 @@ class Application:
         self.txtusuario.delete(0, END)
         self.txtsenha.delete(0, END)
 
+
     def buscarUsuario(self):
         user = Usuarios()
+
         idusuario = self.txtidusuario.get()
 
-        self.lblmsg["text"] = user.selectUser(idusuario)  # Chama o método para buscar o usuário no banco de dados
+        self.lblmsg["text"] = user.selectUser(idusuario) # Chama o método para buscar o usuário no banco de dados
 
         # Preenche os campos com os dados do usuário buscado
         self.txtidusuario.delete(0, END)
