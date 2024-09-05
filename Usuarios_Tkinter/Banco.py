8be29e91e118d9b80ef33a4ab6be028072fcb017
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2024-09-05 01:50:13 -0300

    Update Banco.py

--- Usuarios_Tkinter/Banco.py
+++ Usuarios_Tkinter/Banco.py
@@ -1,19 +1,18 @@
 import sqlite3
 
-class Banco():
+class Banco:
     def __init__(self):
-        self.conexao = sqlite3.connect('banco.db')
+        self.conexao = sqlite3.connect('banco.db')  # Cria/abre o arquivo do banco de dados
         self.createTable()
-        
+
     def createTable(self):
-        c = self.conexao.cursor()
-        c.execute("""create table if not exists usuarios (
-                  idusuario integer primary key autoincrement ,
-                  nome text,
-                  telefone text,
-                  email text,
-                  usuario text,
-                  senha text)""")
-        
+        c = self.conexao.cursor()  # Cria um cursor para executar comandos SQL
+        c.execute("""CREATE TABLE IF NOT EXISTS usuarios (
+                      idusuario INTEGER PRIMARY KEY AUTOINCREMENT,
+                      nome TEXT,
+                      telefone TEXT,
+                      email TEXT,
+                      usuario TEXT,
+                      senha TEXT)""")  # Cria a tabela se ela não existir
         self.conexao.commit()
         c.close()
