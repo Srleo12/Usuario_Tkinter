b0e43eb76c2e5f164e276b307bbe59340203a70c
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2024-09-05 05:46:31 -0300

    Update Usuarios.py

--- Usuarios_Tkinter/Usuarios.py
+++ Usuarios_Tkinter/Usuarios.py
@@ -1,6 +1,9 @@
 from Banco import Banco
 
+
 class Usuarios(object):
+
+    # Atributos que representam os campos do banco de dados
     def __init__(self, idusuario = 0, nome = "", telefone = "", email = "", usuario = "", senha = ""):
         self.info = {}
         self.idusuario = idusuario
@@ -11,13 +14,18 @@ class Usuarios(object):
         self.senha = senha
 
     def insertUser(self):
+
+        
         banco = Banco()
         try:
+
+            # Comando SQL para inserir um novo usuário
             c = banco.conexao.cursor()
             c.execute("insert into usuarios (nome, telefone, email,usuario, senha) values ('" + self.nome + "', '" +
                       self.telefone + "', '" + self.email + "', '" +
                       self.usuario + "', '" + self.senha + "' )")
             
+            # Confirma a inserção
             banco.conexao.commit()
             c.close()
             
@@ -32,8 +40,10 @@ class Usuarios(object):
             
             c = banco.conexao.cursor()
             
+            # Comando SQL para atualizar os dados de um usuário existente
             c.execute("update usuarios set nome = '" + self.nome + "', telefone = '" + self.telefone + "', email = '" + self.email + "', usuario = '" + self.usuario + "', senha = '" + self.senha + "' where idusuario = " + self.idusuario + " ")
 
+            # Confirma a atualização
             banco.conexao.commit()
             c.close()
 
@@ -49,8 +59,10 @@ class Usuarios(object):
             
             c = banco.conexao.cursor()
 
+            # Comando SQL para deletar um usuário com base no id
             c.execute("delete from usuarios where idusuario = " + self.idusuario + " ")
 
+            # Confirma a exclusão
             banco.conexao.commit()
             c.close()
 
@@ -65,8 +77,10 @@ class Usuarios(object):
             
             c = banco.conexao.cursor()
 
+            # Comando SQL para selecionar um usuário com base no id
             c.execute("select * from usuarios where idusuario = " + idusuario + " ")
             
+            # Preenche os atributos com os dados retornados do banco
             for linha in c:
                 self.idusuario = linha[0]
                 self.nome = linha[1]
