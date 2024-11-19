76178d5cf7785351190126f54dda85995cdf3b8a
Srleo12 <carlosnatha2345@gmail.com>
2024-11-19 08:52:19 -0300

    delete

--- converter_png_ico.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-# Abra a imagem que você deseja converter
-img = Image.open("imagem.jpg")  # Altere para o caminho da sua imagem
-
-#  Salve a imagem no formato ICO
-img.save("icone.ico", format="ICO", sizes=[(32, 32)])  # Define o tamanho do ícone (32x32 no exemplo)
